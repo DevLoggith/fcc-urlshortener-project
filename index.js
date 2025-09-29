51c4ba5a3d16e969a82b67b1b1af731a9d31409a
logan <92327020+DevLoggith@users.noreply.github.com>
2025-09-29 12:43:26 -0400

    import body-parser & create basic post route logic
    - create proper imports for db & schemas files
    - import nanoid & body parser
    - change the order of routes
    - update package-lock.json with nanoid installation

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const connectDB = require("db");
-const Url = require("schemas");
+const {nanoid} = require("nanoid");
+let bodyParser = require("body-parser");
+
+const connectDB = require("./db");
+const Url = require("./schemas");
 
 const app = express();
 connectDB();
@@ -14,17 +17,31 @@ app.use(cors());
 
 app.use("/public", express.static(`${process.cwd()}/public`));
 
+app.use("/", bodyParser.urlencoded({extended: false}));
+
 app.get("/", function (req, res) {
 	res.sendFile(process.cwd() + "/views/index.html");
 });
 
+app.post("/api/shorturl", (req, res) => {
+	// search db to see if req.body.url already exists
+	// if so, return json of that document
+	// in not, create new Url document and save it
+	const url = new Url ({
+		original_url: req.body.url,
+		short_url: nanoid(5)
+	});
+
+	try {
+		url.save();
+		res.status(201).json(url);
+	} catch (err) {
+		res.status(400).json({message: err.message});
+	}
+});
 
 app.get("/api/shorturl/:surl", (req, res) => {
 
-});
-
-app.post("/api/shorturl", (req, res) => {
-	
 });
 
 app.listen(port, function () {
