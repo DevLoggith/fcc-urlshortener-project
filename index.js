35289ce641f9c645bd945792c529e76fcab335f8
logan <92327020+DevLoggith@users.noreply.github.com>
2025-10-03 19:55:24 -0400

    add try/catch block to short URL DB search
    assorted formatter fixes

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const dns = require("dns").promises;
 const connectDB = require("./db");
 const UrlDoc = require("./schemas");
 
-
 const app = express();
 connectDB();
 
@@ -32,9 +31,9 @@ app.post("/api/shorturl", async (req, res) => {
 	}
 
 	const urlObject = new URL(req.body.url);
-    if (urlObject.protocol != "http:" && urlObject.protocol != "https:") {
-        return res.json({ error: "invalid url" });
-    }
+	if (urlObject.protocol != "http:" && urlObject.protocol != "https:") {
+		return res.json({ error: "invalid url" });
+	}
 
 	// check that the URL resolves to an actual site
 	try {
@@ -45,19 +44,24 @@ app.post("/api/shorturl", async (req, res) => {
 
 	try {
 		// search db for inputted URL (minus the id and doc version fields)
-		const existingDoc = await UrlDoc.findOne({original_url: req.body.url}).select("-_id -__v");
+		const existingDoc = await UrlDoc.findOne({
+			original_url: req.body.url,
+		}).select("-_id -__v");
 		if (existingDoc) {
 			return res.json(existingDoc);
 		}
 
 		const url = new UrlDoc({
 			original_url: req.body.url,
-			short_url: Math.floor(Math.random() * (99999 - 10000 + 1)) + 10000
+			short_url: Math.floor(Math.random() * (99999 - 10000 + 1)) + 10000,
 		});
 
 		const savedURL = await url.save();
 
-		res.status(201).json({original_url: savedURL.original_url, short_url: savedURL.short_url});
+		res.status(201).json({
+			original_url: savedURL.original_url,
+			short_url: savedURL.short_url,
+		});
 	} catch (err) {
 		res.status(400).json({ message: err.message });
 	}
@@ -65,14 +69,22 @@ app.post("/api/shorturl", async (req, res) => {
 
 app.get("/api/shorturl/:nanourl", async (req, res) => {
 	if (req.params.nanourl.length !== 5) {
-		return res.json({error: "Short URL needs to be 5 characters long"})
+		return res.json({ error: "Short URL needs to be 5 characters long" });
 	}
 
-	const existingDoc = await UrlDoc.findOne({short_url: req.params.nanourl})
-	if (existingDoc) {
-		return res.redirect(existingDoc.original_url);
-	} else {
-		return res.json({error: "No short URL found for the given input"});
+	try {
+		const existingDoc = await UrlDoc.findOne({
+			short_url: req.params.nanourl,
+		});
+		if (existingDoc) {
+			return res.redirect(existingDoc.original_url);
+		} else {
+			return res.json({
+				error: "No short URL found for the given input",
+			});
+		}
+	} catch (err) {
+		res.status(400).json({ message: err.message });
 	}
 });
 
